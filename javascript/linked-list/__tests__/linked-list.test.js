7ce1feaa1e362b20de4486d74278aa9d6a23fec0
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-03-05 12:32:24 +0300

    solved

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,66 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+const { describe, it } = require('eslint/lib/rule-tester/rule-tester');
+//const { expect } = require('@jest/globals');
+const LinkedList = require('../linked-list.js');
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
-  });
-});
+describe('testing Linked List', () => {
+    it('test creating an LL instance', () => {
+        const ll = new LinkedList();
+        // expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll).toBeDefined();
+        expect(ll.head).toBeNull();
+    })
+})
+
+describe('insert to the beginning of the LL', () => {
+    // we have two cases
+    // 1. if the LL is empty
+    it('add to an empty LL', () => {
+        const ll = new LinkedList();
+        ll.insert('a'); // create a new node (value=a)
+        expect(ll.head.value).toEqual('a');
+        expect(ll.head.next).toBeNull();
+
+    })
+    // 2. if the LL is NOT empty
+    it('add to a non-empty LL', () => {
+        const ll = new LinkedList();
+        ll.insert('a');
+        ll.insert('b');
+        expect(ll.head.value).toEqual('b');
+        expect(ll.head.next.value).toEqual('a');
+
+    })
+})
+
+describe("check if it exists or not ", () => {
+
+    it("lets check", () => {
+        const ll = new LinkedList();
+        ll.insert('a');
+        ll.insert('b');
+        expect(ll.includes('a')).toBe(true);
+        expect(ll.includes('b')).toBe(true);
+        expect(ll.includes('c')).toBeFalsy();
+
+    })
+
+})
+
+
+//"{ a } -> { b } -> { c } -> NULL"
+
+describe("get string  ", () => {
+
+    it("get string ", () => {
+        const ll = new LinkedList();
+        ll.append('a');
+        ll.append('b');
+        ll.append('c');
+        expect(ll.toString()).toBe("{ a } -> { b } -> { c } -> NULL");
+        
+
+    })
+
+})
---
+++ javascript/linked-list/readMe.md
@@ -0,0 +1,25 @@
+# Singly Linked List
+<!-- Short summary or background information -->
+-how to create linked list 
+-how to add node 
+-how to create pointers 
+-how to loop through linked list nodes
+## Challenge
+<!-- Description of the challenge -->
+i created a linked list class with function (Insert , append , includes, toString )
+insert ===> add node to start of linked list
+append ===> add node to end of linked list
+includes ===> check if value exists in the linked list or not 
+toString ===> return all values of the nodes in the linked list 
+
+## Approach & Efficiency
+ What approach did you take?  while loop && if statments 
+  Why? to loop through linked list nodes and check for the condition  
+  What is the Big O space/time for this approach? Big O(n)  
+
+## API
+<!-- Description of each method publicly available to your Linked List -->
+insert ===> add node to start of linked list
+append ===> add node to end of linked list
+includes ===> check if value exists in the linked list or not 
+toString ===> return all values of the nodes in the linked list 
