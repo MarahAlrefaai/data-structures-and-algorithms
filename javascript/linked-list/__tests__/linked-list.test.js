e1a960d04a0c2486fb09f8109f1a63841372a402
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-03-11 22:02:33 +0300

    solved

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -69,7 +69,7 @@ describe("check if it exists or not ", () => {
        expect(ll.insertafter(8,5)).toBe("No change, method exception");
        //expect(ll.insertbefore(6,5)).toBe("empty");
     })*/
-    it("Kth method ", () => {
+    /*it("Kth method ", () => {
         const ll = new LinkedList();
         ll.insert(2);
         ll.insert(8);
@@ -83,6 +83,28 @@ describe("check if it exists or not ", () => {
        expect(ll.kth(1)).toBe(8);
        expect(ll.kth(-1)).toBe("exception");
        
+    })*/
+    it("zip method ", () => {
+        const ll1 = new LinkedList();
+        const ll2 = new LinkedList();
+        ll1.insert(3);
+        ll1.insert(2);
+        ll1.insert(1);
+        console.log("l 1   : "+ll1.toString());
+        ll2.insert(6);
+        ll2.insert(5);
+        ll2.insert(4);
+        console.log("l 2   : "+ll2.toString());
+        console.log("final : "+ ll1.zipLists(ll1,ll2));
+
+        //ll1 < ll2
+        //expect(ll1.zipLists(ll1,ll2)).toBe("{ 1 } -> { 4 } -> { 2 } -> { 5 } -> { 6 } -> NULL")
+
+        //ll1 > ll2
+        //expect(ll1.zipLists(ll1,ll2)).toBe("{ 1 } -> { 4 } -> { 2 } -> { 5 } -> { 3 } -> NULL")
+
+       //ll1 && ll2 equal length
+      // expect(ll1.zipLists(ll1,ll2)).toBe("{ 1 } -> { 4 } -> { 2 } -> { 5 } -> { 3 } -> { 6 } -> NULL")
     })
 
 })
---
+++ javascript/linked-list/readme-ziplists.md
@@ -0,0 +1,12 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+create function that  merge 2 lists together  
+list1 ==> 1=>2=>3
+list2 ==> 4=>5=>6
+zip==>1=>4=>2=>5=>3>6
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![alt text](ziplist.jpg)
+## Approach & Efficiency
+What approach did you take? while loop to loop in the list and add nodes 
+ What is the Big O space/time for this approach?  BigO time (n)   BigO space (3)
