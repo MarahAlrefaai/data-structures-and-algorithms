5db2e7d70fb1a2a8c42dab6d0e0628d0653ba07f
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-03-17 13:38:41 +0300

    solved

---
+++ javascript/stack-and-queue/__test__/pseudoQqueue.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+const pseudoQueue = require('../pseudo-queue');
+
+describe('enqueue pseudoQueue', () => {
+ 
+  it('add to queue', () => {
+    const pseudoqueue = new pseudoQueue();
+    pseudoqueue.enqueue(1);
+    pseudoqueue.enqueue(2);
+  console.log("after add to pseudoqueue 1111"+pseudoqueue.stack1.tostring());  
+  console.log("after add to pseudoqueue 2222"+pseudoqueue.stack2.tostring());  
+      
+  })
+
+  it('add to queue', () => {
+    const pseudoqueue = new pseudoQueue();
+    pseudoqueue.enqueue(1);
+    pseudoqueue.enqueue(2);
+    console.log("after add to pseudoqueue"+pseudoqueue.stack1.tostring()); 
+    
+    pseudoqueue.dequeue();
+    console.log("after delete 11111 to pseudoqueue"+pseudoqueue.stack1.tostring()); 
+    console.log("after delete 22222 to pseudoqueue"+pseudoqueue.stack2.tostring()); 
+    
+  })})
