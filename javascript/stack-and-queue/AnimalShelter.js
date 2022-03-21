0e48d7a2b55c0bd5f9adae205eac0dd20cda5707
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-03-21 13:52:38 +0300

    solved

---
+++ javascript/stack-and-queue/AnimalShelter.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Queue = require('./queue.js');
+class AnimalShelter {
+  constructor(){
+  this.name="";
+  this.queue=new Queue(); }
+  
+//------------------------------------------
+//enqueue
+enqueue(animal){
+if(typeof animal=="object"){ this.queue.enqueue(animal); }
+else{ return "its not an object";   }
+
+}
+
+//-------------------------------------
+//dequeue
+
+dequeue(pref){
+  if(this.queue.peek().name==pref)
+{  
+  let aniName=this.queue.peek();
+  this.queue.dequeue();
+  return aniName;
+ }else{ return null;   }}}
+
+
+ //------------------------------------
+ //cat
+class Cat extends AnimalShelter {
+  constructor(name){super();this.name=name;}
+  animalName(){ return this.name;}}
+//--------------------------------------
+//dog
+class Dog extends AnimalShelter {
+  
+  constructor(name){super();this.name=name;}
+  animalName(){return this.name; }}
+
+
+module.exports = {AnimalShelter,Cat,Dog}
---
+++ javascript/stack-and-queue/__test__/AnimalShelter.test.js
@@ -0,0 +1,40 @@
+'use strict'
+const {AnimalShelter,Cat,Dog}= require('../AnimalShelter.js');
+describe('testing creation AnimalShelter 1 ', () => {
+  it('test creating an AnimalShelter instance', () => {
+      const Animal = new AnimalShelter();
+      expect(Animal).toBeDefined();     
+  })})
+describe('test AnimalShelter 2',()=>{
+
+  it('test adding cat object ',async()=>{
+    
+    const animal =new AnimalShelter();
+    const cat =new Cat("cat");
+    
+    animal.enqueue(cat);
+    expect(animal.queue.length).toBe(1);
+    console.log(animal.queue.front);
+    expect((animal.queue.peek()).animalName()).toBe("cat");
+    console.log(animal.queue.front);
+  })
+  it('test adding dog object ',async()=>{
+    const animal =new AnimalShelter();
+    const dog =new Dog("dog");
+    animal.enqueue(dog);
+    expect(animal.queue.length).toBe(1);
+    expect(animal.queue.peek().animalName()).toBe("dog");
+    console.log(animal.queue.front);
+    expect(animal.enqueue("cat")).toBe("its not an object");
+  })
+  it('deleting object ',async()=>{
+    const animal =new AnimalShelter();
+    const dog =new Dog("dog");
+    animal.enqueue(dog);
+    expect(animal.queue.length).toBe(1);
+    expect(animal.queue.peek().animalName()).toBe("dog");
+    expect(typeof (animal.dequeue("dog"))).toBe("object");
+    expect(animal.queue.length).toBe(0);
+  })
+
+})
