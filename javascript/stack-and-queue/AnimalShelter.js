d02f4031735395268972275e079b34250711e3a4
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-03-21 14:26:23 +0300

    solved

--- javascript/stack-and-queue/AnimalShelter.js
+++ javascript/stack-and-queue/AnimalShelter.js
@@ -5,27 +5,20 @@ class AnimalShelter {
   constructor(){
   this.name="";
   this.queue=new Queue(); }
-  
 //------------------------------------------
 //enqueue
 enqueue(animal){
 if(typeof animal=="object"){ this.queue.enqueue(animal); }
-else{ return "its not an object";   }
-
-}
-
+else{ return "its not an object";   }}
 //-------------------------------------
 //dequeue
-
 dequeue(pref){
   if(this.queue.peek().name==pref)
 {  
   let aniName=this.queue.peek();
   this.queue.dequeue();
   return aniName;
- }else{ return null;   }}}
-
-
+ }else{ return null;}}}
  //------------------------------------
  //cat
 class Cat extends AnimalShelter {
@@ -34,9 +27,6 @@ class Cat extends AnimalShelter {
 //--------------------------------------
 //dog
 class Dog extends AnimalShelter {
-  
   constructor(name){super();this.name=name;}
   animalName(){return this.name; }}
-
-
 module.exports = {AnimalShelter,Cat,Dog}
---
+++ javascript/stack-and-queue/AnimalSilter.readme.md
@@ -0,0 +1,10 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+create 2 classes (dog , cat ) extended from the parent class Animal that contains enqueue and dequeue to add and delete objects of cat and dogs
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![alt text](animalshelter.jpg)
+## Approach & Efficiency
+ What approach did you take? Why? What is the Big O space/time for this approach? 
+ enqueue ===> Big O time && space (1)
+ dequeue ===> Big O time (3) && space (1)
